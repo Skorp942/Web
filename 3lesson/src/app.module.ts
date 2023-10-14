5f1484c6ff356759916d1941f3ceb651127b9748
Skorp942 <Stalker22294_94@mail.ru>
2023-10-14 19:53:26 +0700

    Вроде запустилось

--- 3lesson/src/app.module.ts
+++ 3lesson/src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from './students/student.entity';
-import { StudentsModule } from './students/students.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    TypeOrmModule.forFeature([Student]),
-    StudentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: "postgres",
+      password: "1111",
+      database: "Students",
+      entities: [Student], // Add other entities if needed
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
